ef6a3e1f63380eba390f6fc2c0034965e0c71ab2
Matthew Asaminew <mattasaminew@gmail.com>
2018-09-18 02:53:05 -0400

    Add rover class, input file, update readme

---
+++ rover.rb
@@ -0,0 +1,90 @@
+class Rover
+	COMPASS_DIRECTIONS = %w(N E S W).freeze # must be in clockwise order
+	VALID_CMDS = %w(L R M).freeze
+
+  attr_accessor :mission
+	attr_reader :completed, :result, :errors
+
+  def initialize x, y, dir, cmd_string
+		@completed = false
+		@result = nil
+		@errors = []
+
+		@cmd_string = cmd_string
+		@loc_x = x
+		@loc_y = y
+
+		if COMPASS_DIRECTIONS.index(dir)
+			@direction = dir
+		else
+			@errors << "Invalid direction: #{dir}"
+		end
+  end
+
+	def run_commands
+		if errors.empty?
+			@cmd_string.split('').each do |cmd|
+				command_rover(cmd)
+				break if @errors.last == "Out of bounds"
+			end
+
+			@result = "#{@loc_x} #{@loc_y} #{@direction}"
+		end
+
+		@completed = true
+	end
+
+	private
+
+	def command_rover cmd
+		if VALID_CMDS.index(cmd)
+			case cmd
+			when 'L', 'R'
+				turn_rover(cmd)
+			when 'M'
+				move_rover
+			end
+		else
+			@errors << "Invalid command: #{cmd}"
+		end
+	end
+
+	def turn_rover turn_direction
+		i = COMPASS_DIRECTIONS.index(@direction)
+
+		if turn_direction == 'R'
+			if i < COMPASS_DIRECTIONS.length - 1
+				i += 1
+			else
+				i = 0
+			end
+		elsif turn_direction == 'L'
+			if i > 0
+				i -= 1
+			else
+				i = COMPASS_DIRECTIONS.length - 1
+			end
+		end
+
+		@direction = COMPASS_DIRECTIONS[i]
+	end
+
+	def move_rover
+		x, y = @loc_x, @loc_y
+
+		case @direction
+		when 'N'
+			@loc_y += 1 if !mission || (mission && (@loc_y < mission.max_y))
+		when 'E'
+			@loc_x += 1 if !mission || (mission && (@loc_x < mission.max_x))
+		when 'S'
+			@loc_y -= 1 if @loc_y > 0
+		when 'W'
+			@loc_x -= 1 if @loc_x > 0
+		end
+
+		if x == @loc_x && y == @loc_y
+			@errors << "Out of bounds"
+		end
+	end
+end
